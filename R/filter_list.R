85c08b81c59b771eb314db64742d01801978cf00
jordanvazquez <jordan@famillevazquez.ch>
2023-12-07 11:50:40 +0100

    including shiny dummy into package

--- R/filter_list.R
+++ R/filter_list.R
@@ -1,27 +1,26 @@
-colnames(cocktails) <- colnames(cocktails) %>% str_trim()
-
-filter_list <- function(alcohol = NULL, ing1 =NULL, ing2 = NULL){
-  #req(input$alcohol, input$ing1, input$ing2)
-  if(!all(c(alcohol, ing1, ing2) %in% colnames(cocktails))){
-    stop(cat("Invalid Input"))
-  }
-
-  if(is.null(alcohol)){
-    cat("The base alcohol selection is required")
-  }else{
-    if(is.null(ing1) && is.null(ing2)){
-      df <- cocktails %>%
-        dplyr::filter(!is.na(.[[alcohol]]))
-    }else if(is.null(ing1) && !is.null(ing2)){
-      df <- cocktails %>%
-        dplyr::filter(!is.na(.[[alcohol]]), !is.na(.[[ing2]]))
-    }else if(!is.null(ing1) && is.null(ing2)){
-      df <- cocktails %>%
-        dplyr::filter(!is.na(.[[alcohol]]), !is.na(.[[ing1]]))
-    }else{
-      df <- cocktails %>%
-        dplyr::filter(!is.na(.[[alcohol]]), !is.na(.[[ing1]]), !is.na(.[[ing2]]))
-    }
-  }
-  return(df %>% select(Name))# %>% kableExtra::kbl())
-}
+# filter_list <- function(alcohol = NULL, ing1 =NULL, ing2 = NULL){
+#   colnames(cocktails) <- colnames(cocktails) %>% str_trim()
+#   #req(input$alcohol, input$ing1, input$ing2)
+#   if(!all(c(alcohol, ing1, ing2) %in% colnames(cocktails))){
+#     stop(cat("Invalid Input"))
+#   }
+#
+#   if(is.null(alcohol)){
+#     cat("The base alcohol selection is required")
+#   }else{
+#     if(is.null(ing1) && is.null(ing2)){
+#       df <- cocktails %>%
+#         dplyr::filter(!is.na(.[[alcohol]]))
+#     }else if(is.null(ing1) && !is.null(ing2)){
+#       df <- cocktails %>%
+#         dplyr::filter(!is.na(.[[alcohol]]), !is.na(.[[ing2]]))
+#     }else if(!is.null(ing1) && is.null(ing2)){
+#       df <- cocktails %>%
+#         dplyr::filter(!is.na(.[[alcohol]]), !is.na(.[[ing1]]))
+#     }else{
+#       df <- cocktails %>%
+#         dplyr::filter(!is.na(.[[alcohol]]), !is.na(.[[ing1]]), !is.na(.[[ing2]]))
+#     }
+#   }
+#   return(df %>% select(Name))# %>% kableExtra::kbl())
+# }
--- R/show_recipe.R
+++ R/show_recipe.R
@@ -1,15 +1,15 @@
-show_recipe <- function(selected){
-  df <- cocktails %>%
-    filter(Name == selected)
-
-  type <- df$Type
-  glass <- df$Glass
-  recipe <- df$Recipe
-
-  ing_table <- df %>%
-    select(-c(Type, Category, Picture, Glass, Recipe)) %>%
-    pivot_longer(-Name, names_to = "ingredient", values_to = "quantity") %>%
-    drop_na() %>%
-    select(-Name)
-  return(ing_table)
-}
+# show_recipe <- function(selected){
+#   df <- cocktails %>%
+#     filter(Name == selected)
+#
+#   type <- df$Type
+#   glass <- df$Glass
+#   recipe <- df$Recipe
+#
+#   ing_table <- df %>%
+#     select(-c(Type, Category, Picture, Glass, Recipe)) %>%
+#     pivot_longer(-Name, names_to = "ingredient", values_to = "quantity") %>%
+#     drop_na() %>%
+#     select(-Name)
+#   return(ing_table)
+# }
