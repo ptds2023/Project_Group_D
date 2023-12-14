70bcb5c2c93ae1841263548856a1a4e82766c46d
jordanvazquez <127729476+jordanvazquez@users.noreply.github.com>
2023-12-14 15:32:00 +0100

    documenting cocktails.rda dataset

---
+++ R/data.R
@@ -0,0 +1,18 @@
+#' Drinks dataset with ingredients
+#'
+#' A dataset which contains drink names, images and ingredients information.
+#'
+#' @format ## `cocktails`
+#' A data frame with 534 rows and 310 columns:
+#' \describe{
+#'   \item{Name}{Drink name}
+#'   \item{Type}{Type of drink (i.e., alcoholic, non-alcoholic, etc.)}
+#'   \item{Category}{General category of the drink (cocktail, beer, shot)}
+#'   \item{Picture}{URL to the picture of the drink}
+#'   \item{Glass}{Type of glass recommended for the drink}
+#'   \item{Recipe}{Text description of the recipe to make the drink}
+#'   \item{304 ingredient}{All other columns which contain the amoung of each ingredients if present in the drink, and NAs if not}
+#'   ...
+#' }
+#' @source <https://www.kaggle.com/datasets/ai-first/cocktail-ingredients>
+"cocktails"
