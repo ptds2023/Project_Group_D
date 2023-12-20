26bc18433a2b77cd151ab06b610db94b791a5900
jordanvazquez <127729476+jordanvazquez@users.noreply.github.com>
2023-12-20 13:55:59 +0100

    modified ingredientsTable function
    Changed from xtable to kable to render the table of ingredient and quantities

--- R/ingredientsTable.R
+++ R/ingredientsTable.R
@@ -1,27 +1,29 @@
 #' Render Ingredient-Quantity Table
 #'
-#' The ingredientsTable function takes as input the cocktails dataframe and the
+#' The \code{ingredientsTable} function takes as input the cocktails dataframe and the
 #' name of a cocktail in that dataframe and returns its ingredients along with quantities.
+#' @param df The cocktails dataframe from the package
 #' @param selected_cocktail A cocktail name string
-#' @param df The cocktails dataframe
 #'
-#' @return An html table with ingredients and corresponding quantities
+#' @return An HTML table with ingredients and corresponding quantities
 #' @importFrom rlang .data
 #' @export
 #'
 #' @examples
 #' ingredientsTable(cocktails, "daiquiri")
-ingredientsTable <- function(df, selected_cocktail) {
-  # Check if df is NULL or empty, or if the selected cocktail is not in the df
-  if (is.null(df) || nrow(df) == 0 || !(selected_cocktail %in% df$Name)) {
-    return(xtable::xtable(data.frame(Ingredient = NA, Quantity = NA)))
+ingredientsTable <- function(df = cocktails, selected_cocktail) {
+  # Check if the selected cocktail is not in the df
+  if(!(selected_cocktail %in% df$Name)){
+    stop("This cocktail is not in the dataframe")
   }
   #creating table
   ing_table <- df %>%
     dplyr::filter(.data$Name == selected_cocktail) %>%
     dplyr::select(-c(.data$Type, .data$Category, .data$Picture, .data$Glass, .data$Recipe)) %>%
     tidyr::pivot_longer(-.data$Name, names_to = "Ingredient", values_to = "Quantity", values_drop_na = TRUE) %>%
-    dplyr::select(-.data$Name)
+    dplyr::select(-.data$Name) %>%
+    kableExtra::kbl() %>%
+    kableExtra::kable_styling("striped")
 
-  return(xtable::xtable(ing_table))
+  return(ing_table)
 }
